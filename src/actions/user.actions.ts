d9637912ae4d6abce2413d2204f4826d66cd5a78
evanwilson2123 <evanmwilson@gmail.com>
2024-10-09 11:45:11 -0400

    test

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -4,7 +4,11 @@ import { connectDB } from "@/lib/db";
 export const createUser = async (user: any) => {
   try {
     await connectDB();
-    const newUser = await User.create(user);
+    const newUser = new User({
+      clerkId: user.id,
+      email: user.email,
+    });
+    await newUser.save();
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
     console.log(error);
